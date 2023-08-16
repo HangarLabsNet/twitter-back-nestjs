fb98c11cae5a9b3f7e68ee89d37ccc963761385b
Luis <lvercellih@gmail.com>
2023-08-16 12:44:49 -0500

    Swagger configuration

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,20 +2,18 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 
 export const setupSwagger = function(app) {
-  if (process.env.ENABLE_SWAGGER === '') {
-    return
-  }
-
-  const config = new DocumentBuilder()
-    .setTitle("Twitter API")
-    .setDescription("The Twitter API description")
-    .setVersion("1.0")
-    .addTag("posts")
-    .build();
-
-  SwaggerModule.setup(
-    process.env.SWAGGER_PATH ?? '', 
-    app, 
-    SwaggerModule.createDocument(app, config)
-  );
+  if (process.env.SWAGGER_PATH) {
+    const config = new DocumentBuilder()
+      .setTitle("Twitter API")
+      .setDescription("The Twitter API description")
+      .setVersion("1.0")
+      .addTag("posts")
+      .build();
+  
+      SwaggerModule.setup(
+        process.env.SWAGGER_PATH, 
+        app, 
+        SwaggerModule.createDocument(app, config)
+      );
+    }
 }
