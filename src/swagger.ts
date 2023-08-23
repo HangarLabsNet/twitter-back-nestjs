6f63fd04464c5f9b0f1fcff48b45d27dfa3f061c
Luis <lvercellih@gmail.com>
2023-08-22 21:35:45 -0500

    Test deployment

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,19 +2,23 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export const setupSwagger = function(app) {
   if (process.env.SWAGGER_PATH) {
-    const config = new DocumentBuilder()
+    const builder = new DocumentBuilder()
       .setTitle("Twitter API")
       .setDescription("The Twitter API description")
-      .setVersion("1.0")
-      .setBasePath(process.env.SWAGGER_API_BASE_PATH ?? '')
-      .build();
-  
-      SwaggerModule.setup(
-        process.env.SWAGGER_PATH, 
-        app, 
-        SwaggerModule.createDocument(app, config)
-      );
+      .setVersion("1.0");
+    
+    if (process.env.SWAGGER_API_BASE_PATH) {
+      builder.addServer(process.env.SWAGGER_API_BASE_PATH)
     }
+    
+    const config = builder.build();
+  
+    SwaggerModule.setup(
+      process.env.SWAGGER_PATH, 
+      app, 
+      SwaggerModule.createDocument(app, config)
+    );
+  }
 }
 
 // https://pietrzakadrian.com/blog/how-to-create-pagination-in-nestjs-with-typeorm-swagger
